a2220bd54fa830035bd1534ba0558595283ce4f6
Evaldas <ewaldas@gmail.com>
2016-05-23 23:13:10 +0100

    Added proper main

---
+++ program.py
@@ -0,0 +1,21 @@
+
+def main():
+    # print the header
+    print_the_header()
+    # get postcode from user
+    # get html from web
+    # parse the html
+    # display for the forecast
+    print('hello from main')
+
+
+def print_the_header():
+    print('----------------------------')
+    print('       WEATHER APP')
+    print('----------------------------')
+    print()
+
+
+if __name__ == '__main__':
+    main()
+
