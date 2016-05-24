54f2e4650299532d918d92af9e07115b0729f270
Evaldas <ewaldas@gmail.com>
2016-05-24 23:00:06 +0100

    Added code to get html from web We query for postcode in BR2+7PD format

--- program.py
+++ program.py
@@ -2,10 +2,10 @@
 def main():
     print_the_header()
 
-    code = input('What zipcode do you want the weather for (BR2)? ')
+    code = input('What zipcode do you want the weather for (BR2+7PD)? ')
 
     # get html from web
-    print(code)
+    get_html_from_web(code)
     # parse the html
     # display for the forecast
     print('hello from main')
@@ -17,6 +17,10 @@ def print_the_header():
     print('----------------------------')
     print()
 
+
+def get_html_from_web(postcode):
+    url = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query={}'.format(postcode)
+    print(url)
 
 if __name__ == '__main__':
     main()
